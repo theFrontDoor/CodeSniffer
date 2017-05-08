db442445deefb2c1748b35c29090b729f602ccb5
Kevin Meijer <kevin.meijer@thefrontdoor.nl>
2017-05-08 09:41:00 +0200

    Added a helper script

---
+++ Scripts/update_repo.php
@@ -0,0 +1,23 @@
+#!/usr/bin/env php
+<?php
+
+$reposToPull = array(
+    '~/.bin/phpcs' => 'origin master',
+    '~/.bin/phpcs/src/Standards/TFD' => 'origin master',
+);
+
+echo('Updating repositories..' . PHP_EOL);
+
+foreach ($reposToPull as $repo => $target) {
+
+    echo(' - ' . $repo . ($target ? ' @ ' . $target : '') . PHP_EOL);
+
+    exec('git --git-dir ' . $repo . '/.git --work-tree ' . $repo . '/ pull ' . $target, $output, $exitCode);
+    if ($exitCode > 0) {
+        echo('Update failed with exit code ' . $exitCode . PHP_EOL);
+        exit($exitCode);
+    }
+
+}
+
+echo('Done.' . PHP_EOL);
