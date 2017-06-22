1c0b54f13b0cfc51d7510858cfd934354f248fc0
Kevin Meijer <kevin.meijer@thefrontdoor.nl>
2017-06-22 07:03:48 +0200

    Added sniffing for class prefixes

---
+++ Sniffs/NamingConventions/ValidClassNameSniff.php
@@ -0,0 +1,50 @@
+<?php
+
+class TFD_Sniffs_NamingConventions_ValidClassNameSniff extends PEAR_Sniffs_NamingConventions_ValidClassNameSniff {
+
+    public function register() {
+        return array_merge(parent::register(), [
+            T_CLASS,
+            T_INTERFACE,
+            T_TRAIT,
+        ]);
+
+    }//end register()
+
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr) {
+
+        var_dump($stackPtr);
+
+        $tokens = $phpcsFile->getTokens();
+
+        // var_dump($tokens);
+
+        $classNamePtr = $phpcsFile->findNext(T_STRING, $stackPtr);
+        $className    = trim($tokens[$classNamePtr]['content']);
+
+        if ($tokens[$stackPtr]['type'] === 'T_INTERFACE') {
+
+            if ($className[0] !== 'I') {
+                $phpcsFile->addError('Interface "' . $className . '" must be prefixed with an I', $stackPtr, 'InterfaceNoI');
+            }
+
+        } elseif ($tokens[$stackPtr]['type'] === 'T_TRAIT') {
+
+            if ($className[0] !== 'T') {
+                $phpcsFile->addError('Trait "' . $className . '" must be prefixed with an T', $stackPtr, 'TraitNoT');
+            }
+
+        } else {
+
+            $abstract = $phpcsFile->findPrevious(T_ABSTRACT, $classNamePtr);
+            $isAbstract = $tokens[$abstract]['line'] === $tokens[$classNamePtr]['line'];
+
+            if ($isAbstract && $className[0] !== 'A') {
+                $phpcsFile->addError('Abstract class "' . $className . '" must be prefixed with an A', $stackPtr, 'AbstractClassNoA');
+            }
+
+        }
+
+    }
+
+}
